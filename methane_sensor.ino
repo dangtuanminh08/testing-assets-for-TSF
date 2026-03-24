825f9c667036e6de5df80281aed59855a5eb8ad6
Minh D <71281116+dangtuanminh08@users.noreply.github.com>
2026-03-24 13:41:02 -0400

    Files
    bme_sensor.ino is empty, will add later

---
+++ methane_sensor.ino
@@ -0,0 +1,28 @@
+int mq4Pin= A0;
+float sensorResistance = 6.93;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(mq4Pin);
+  float voltage = sensorValue * (5.0/1023.0);
+
+  float resistance = (5.0 - voltage) / voltage;
+  float ratio = resistance/sensorResistance;
+
+  float ppm = pow(10, (-0.39 * log10(ratio) + 1.41));
+
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorValue);
+  Serial.print(" | Voltage: ");
+  Serial.print(voltage);
+  Serial.print(" | Resistance (k ohms): ");
+  Serial.print(resistance);
+  Serial.print(" | Ratio : ");
+  Serial.print(resistance/sensorResistance);
+  Serial.print(" | PPM (methane): ");
+  Serial.println(ppm);
+
+}
---
+++ rpm_counter.ino
@@ -0,0 +1,31 @@
+const int IR_PIN = 2;
+volatile unsigned int counter = 0; 
+unsigned long previousMs = 0;
+unsigned int rpm = 0;
+
+void IRinterrupt() {
+  counter++;
+}
+
+void setup() {
+  pinMode(IR_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(IR_PIN), IRinterrupt, FALLING);
+  Serial.begin(9600); 
+}
+
+void loop() {
+  unsigned long currentMs = millis();
+
+  if (currentMs - previousMs >= 1000) {
+    noInterrupts();
+    unsigned int pulses = counter;
+    counter = 0;
+    interrupts();
+
+    rpm = pulses * 60;
+    previousMs = currentMs;
+
+    Serial.print("RPM: ");
+    Serial.println(rpm);
+  }
+}
